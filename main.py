added action backpropagation, fixed bugs

main.py
@@ -14,6 +14,7 @@ from torch.utils.tensorboard import SummaryWriter
 # negative samples:
 # progress: [0-1]
 # pos: [-0.1-0.1]
+# gripper: [-1-1]
 # rot: [-1-1]
 
 NAME = 'train-ibc-basic'
@@ -23,7 +24,7 @@ BATCH_SIZE = 32
 N_NEG = 8
 
 EPOCHS = 300
-EVAL_EVERY = 10
+EVAL_EVERY = 1
 SAVE_EVERY = 10000
 
 GLOBAL_STEP = 0
@@ -32,23 +33,26 @@ def train(model, optimizer, scheduler, criterion, dataloader, writer, device, ck
     global GLOBAL_STEP
     model.train()
 
-    for (img, sentence, ee_pos, ee_rot, joint_angles, progress) in tqdm(dataloader):
+    for (img, sentence, ee_pos, ee_rot, joint_angles, gripper, progress) in tqdm(dataloader):
+        bsz = progress.size(0)
         img = img.to(device).repeat(1 + N_NEG, 1, 1, 1)
         sentence = sentence.to(device).repeat(1 + N_NEG, 1)
         
         ee_pos = ee_pos.to(device)
         ee_rot = ee_rot.to(device)
         joint_angles = joint_angles.to(device)
+        gripper = gripper.to(device)
         progress = progress.to(device).unsqueeze(-1)
-        action = torch.cat((ee_pos, ee_rot, progress), -1).float()
+        action = torch.cat((ee_pos, ee_rot, gripper, progress), -1).float()
 
-        c_ee_pos = (torch.rand(size=(BATCH_SIZE * N_NEG,) + ee_pos.size()[1:], device=device) - 0.5) * 0.2
-        c_ee_rot = (torch.rand(size=(BATCH_SIZE * N_NEG,) + ee_rot.size()[1:], device=device) - 0.5) * 2
-        c_progress = torch.rand(size=(BATCH_SIZE * N_NEG,) + progress.size()[1:], device=device)
-        c_action = torch.cat((c_ee_pos, c_ee_rot, c_progress), -1).float()
+        c_ee_pos = (torch.rand(size=(bsz * N_NEG,) + ee_pos.size()[1:], device=device) - 0.5) * 0.2
+        c_ee_rot = (torch.rand(size=(bsz * N_NEG,) + ee_rot.size()[1:], device=device) - 0.5) * 2
+        c_gripper = (torch.rand(size=(bsz * N_NEG,) + gripper.size()[1:], device=device) - 0.5) * 2
+        c_progress = torch.rand(size=(bsz * N_NEG,) + progress.size()[1:], device=device)
+        c_action = torch.cat((c_ee_pos, c_ee_rot, c_gripper, c_progress), -1).float()
 
         action = torch.cat((action, c_action), 0)
-        y = torch.cat((torch.ones(BATCH_SIZE), torch.zeros(BATCH_SIZE * N_NEG))).to(device)
+        y = torch.cat((torch.ones(bsz), torch.zeros(bsz * N_NEG))).to(device).unsqueeze(-1)
 
         optimizer.zero_grad()
         y_hat = model(img, sentence, action)
@@ -66,28 +70,33 @@ def train(model, optimizer, scheduler, criterion, dataloader, writer, device, ck
             }
             torch.save(checkpoint, ckpt_path.joinpath(f'{GLOBAL_STEP}.pth'))
 
+        GLOBAL_STEP += 1
+
 def test(model, criterion, dataloader, writer, device):
     with torch.no_grad():
         model.eval()
         total_loss = 0
 
-        for (img, sentence, ee_pos, ee_rot, joint_angles, progress) in tqdm(dataloader):
+        for (img, sentence, ee_pos, ee_rot, joint_angles, gripper, progress) in tqdm(dataloader):
+            bsz = progress.size(0)
             img = img.to(device).repeat(1 + N_NEG, 1, 1, 1)
             sentence = sentence.to(device).repeat(1 + N_NEG, 1)
             
             ee_pos = ee_pos.to(device)
             ee_rot = ee_rot.to(device)
             joint_angles = joint_angles.to(device)
+            gripper = gripper.to(device)
             progress = progress.to(device).unsqueeze(-1)
-            action = torch.cat((ee_pos, ee_rot, progress), -1).float()
+            action = torch.cat((ee_pos, ee_rot, gripper, progress), -1).float()
 
-            c_ee_pos = (torch.rand(size=(BATCH_SIZE * N_NEG,) + ee_pos.size()[1:], device=device) - 0.5) * 0.2
-            c_ee_rot = (torch.rand(size=(BATCH_SIZE * N_NEG,) + ee_rot.size()[1:], device=device) - 0.5) * 2
-            c_progress = torch.rand(size=(BATCH_SIZE * N_NEG,) + progress.size()[1:], device=device)
-            c_action = torch.cat((c_ee_pos, c_ee_rot, c_progress), -1).float()
+            c_ee_pos = (torch.rand(size=(bsz * N_NEG,) + ee_pos.size()[1:], device=device) - 0.5) * 0.2
+            c_ee_rot = (torch.rand(size=(bsz * N_NEG,) + ee_rot.size()[1:], device=device) - 0.5) * 2
+            c_gripper = (torch.rand(size=(bsz * N_NEG,) + gripper.size()[1:], device=device) - 0.5) * 2
+            c_progress = torch.rand(size=(bsz * N_NEG,) + progress.size()[1:], device=device)
+            c_action = torch.cat((c_ee_pos, c_ee_rot, c_gripper, c_progress), -1).float()
 
             action = torch.cat((action, c_action), 0)
-            y = torch.cat((torch.ones(BATCH_SIZE), torch.zeros(BATCH_SIZE * N_NEG))).to(device)
+            y = torch.cat((torch.ones(bsz), torch.zeros(bsz * N_NEG))).to(device).unsqueeze(-1)
 
             y_hat = model(img, sentence, action)
             loss = criterion(y_hat, y)
@@ -98,13 +107,11 @@ def test(model, criterion, dataloader, writer, device):
 if __name__ == '__main__':
     writer = SummaryWriter('runs/' + NAME)
     ckpt_path = Path(CKPT_PATH).joinpath(NAME)
-    ckpt_path.mkdirs(exist_ok=True, parents=True)
+    ckpt_path.mkdir(exist_ok=True, parents=True)
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     model = Backbone()
-    if CKPT is not None:
-        model.load_state_dict(torch.load(CKPT), strict=True)
     model = model.to(device)
 
     img_preprocess = transforms.Compose([
@@ -112,25 +119,33 @@ if __name__ == '__main__':
     ])
 
     train_dset = MujocoDataset(
-        '/home/jjzhao/Documents/IRLab/implicit_behavior_cloning/data/train',
+        '/home/localuser/Documents/jjzhao/mujoco_ur5_robotiq85/data/train',
         img_preprocess=img_preprocess,
     )
 
     train_dataloader = DataLoader(train_dset, batch_size=BATCH_SIZE, shuffle=True, num_workers=8, collate_fn=mujoco_collate_fn)
 
     val_dset = MujocoDataset(
-        '/home/jjzhao/Documents/IRLab/implicit_behavior_cloning/data/val',
+        '/home/localuser/Documents/jjzhao/mujoco_ur5_robotiq85/data/val',
         img_preprocess=img_preprocess,
     )
 
     val_dataloader = DataLoader(val_dset, batch_size=BATCH_SIZE, shuffle=False, num_workers=8, collate_fn=mujoco_collate_fn)
 
-    criterion = InfoNCELoss(BATCH_SIZE, N_NEG)
+    criterion = InfoNCELoss(N_NEG)
+    #criterion = nn.BCEWithLogitsLoss()
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.99)
 
+    if CKPT is not None:
+        model.load_state_dict(torch.load(CKPT)['model'], strict=True)
+        optimizer.load_state_dict(torch.load(CKPT)['optimizer'])
+        test(model, criterion, val_dataloader, writer, device)
+
     for i in range(EPOCHS):
+        print(f"Training epoch {i}")
         train(model, optimizer, scheduler, criterion, train_dataloader, writer, device, ckpt_path)
         
         if i % EVAL_EVERY == 0:
             test(model, criterion, val_dataloader, writer, device)
+

models/basic_model.py
@@ -15,7 +15,8 @@ class VisualEncoder(nn.Module):
         self.encoder = nn.Linear(list(self.resnet.modules())[-1].out_features, embedding_size)
     
     def forward(self, x):
-        x = self.resnet(x)
+        with torch.no_grad():
+            x = self.resnet(x)
         return F.relu(self.encoder(x))
 
 class TaskIdEncoder(nn.Module):
@@ -26,7 +27,8 @@ class TaskIdEncoder(nn.Module):
         self.encoder = nn.Linear(input_size, embedding_size)
     
     def forward(self, x):
-        x = self.clip.encode_text(x).float()
+        with torch.no_grad():
+            x = self.clip.encode_text(x).float()
         return F.relu(self.encoder(x))
 
 class ActionEncoder(nn.Module):
@@ -39,7 +41,7 @@ class ActionEncoder(nn.Module):
         return F.relu(self.encoder(x))
 
 class Backbone(nn.Module):
-    def __init__(self, embedding_size=192, intermediate_size=256, img_size=224, task_size=512, action_size=10):
+    def __init__(self, embedding_size=192, intermediate_size=256, img_size=224, task_size=512, action_size=12):
         super(Backbone, self).__init__()
 
         self.visual_encoder = VisualEncoder(img_size, embedding_size)
@@ -50,7 +52,6 @@ class Backbone(nn.Module):
             nn.Linear(embedding_size * 3, intermediate_size),
             nn.ReLU(),
             nn.Linear(intermediate_size, 1),
-            nn.Sigmoid(),
         )
     
     def forward(self, img, task, action):

models/loss.py
@@ -1,13 +1,15 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class InfoNCELoss(nn.Module):
-    def __init__(self, batch_size, nneg):
+    def __init__(self, nneg):
         super(InfoNCELoss, self).__init__()
-        self.batch_size = batch_size
         self.nneg = nneg
     
     def forward(self, y_hat, y):
-        p = torch.exp(-y_hat[:self.batch_size])
-        pneg = torch.sum(torch.exp(-y_hat[self.batch_size:]).reshape(self.batch_size, self.nneg), axis=-1)
-        return torch.sum(-torch.log(p / (p + pneg)))
+        bsz = y.size(0) // (self.nneg + 1)
+        y_hat = y_hat - torch.min(y_hat)
+        p = torch.exp(-y_hat[:bsz])
+        pneg = torch.sum(torch.exp(-y_hat[bsz:]).reshape(bsz, self.nneg), axis=-1)
+        return torch.sum(-torch.log(p / (p + pneg))) / y.size(0)

utils/data.py
@@ -8,19 +8,19 @@ import torchvision
 import numpy as np
 import torch.nn.functional as F
 
-def gen_sentence(attrs, targets):
-    obj1 = targets.attrs['obj1']
-    obj2 = targets.attrs['obj2']
+def gen_sentence(attrs, objective):
+    obj1 = objective['targets'].attrs['obj1']
+    obj2 = objective['targets'].attrs['obj2']
 
-    color1 = attrs['color'][obj1]
-    color2 = attrs['color'][obj2]
+    color1 = attrs['color'][obj1].attrs['name']
+    color2 = attrs['color'][obj2].attrs['name']
 
-    scale1 = attrs['scale'][obj1]
-    scale2 = attrs['scale'][obj2]
+    scale1 = attrs['scale'][obj1].attrs['name']
+    scale2 = attrs['scale'][obj2].attrs['name']
 
-    action = targets.attrs['action']
+    action = objective.attrs['action']
 
-    return f'{action} the {sclae1} {color1} {obj1} on the {scale2} {color2} {obj2}'
+    return f'{action} the {scale1} {color1} {obj1} on the {scale2} {color2} {obj2}'
 
 # Returns singular states of demos
 class MujocoDataset(Dataset):
@@ -68,7 +68,7 @@ class MujocoDataset(Dataset):
                 img = self.img_preprocess(img)
 
             # only support for one objective currently
-            sentence = clip.tokenize(gen_sentence(f['gen_attrs'], f['objectives']['0']['targets']))[0]
+            sentence = clip.tokenize(gen_sentence(f['gen_attrs'], f['objectives']['0']))[0]
 
             future_step = min(step_idx+self.sample, demo_length - 1)
 
@@ -80,9 +80,12 @@ class MujocoDataset(Dataset):
             ee_rot[1::2] = np.sin(rot)
 
             joint_angles = torch.tensor(f['q'][step_idx] - f['q'][future_step])
+            gripper = torch.empty(2)
+            gripper[0] = np.sin(joint_angles[-1])
+            gripper[1] = np.cos(joint_angles[-1])
             progress = torch.tensor((step_idx + 1) / demo_length)
 
-            return img, sentence, ee_pos, ee_rot, joint_angles, progress
+            return img, sentence, ee_pos, ee_rot, joint_angles, gripper, progress
         
         raise Exception(f'Failed to open file {str(demo.joinpath("states.data"))}')
 
@@ -130,7 +133,7 @@ class MujocoTrajectoryDataset(Dataset):
             img = torchvision.io.read_image(str(self.demos[demo_idx].joinpath(f'imgs/{step_idx}.png'))).float() / 255
 
             # only support for one objective currently
-            sentence = clip.tokenize(gen_sentence(f['gen_attrs'], f['objectives']['0']['targets']))[0]
+            sentence = clip.tokenize(gen_sentence(f['gen_attrs'], f['objectives']['0']))[0]
 
             ee_pos = torch.tensor(f['pos'][step_idx:demo_length:self.sample_every][:self.num_points][:,0])
             ee_rot = torch.tensor(f['rot'][step_idx:demo_length:self.sample_every][:self.num_points][:,0])
@@ -161,13 +164,14 @@ class MujocoTrajectoryDataset(Dataset):
         raise Exception(f'Failed to open file {str(demo.joinpath("states.data"))}')
 
 def mujoco_collate_fn(batch):
-    (img, sentence, ee_pos, ee_rot, joint_angles, progress) = zip(*batch)
+    (img, sentence, ee_pos, ee_rot, joint_angles, gripper, progress) = zip(*batch)
 
     img = torch.stack(img)
     sentence = torch.stack(sentence)
     ee_pos = torch.stack(ee_pos)
     ee_rot = torch.stack(ee_rot)
     joint_angles = torch.stack(joint_angles)
+    gripper = torch.stack(gripper)
     progress = torch.stack(progress)
 
-    return img, sentence, ee_pos, ee_rot, joint_angles, progress
+    return img, sentence, ee_pos, ee_rot, joint_angles, gripper, progress